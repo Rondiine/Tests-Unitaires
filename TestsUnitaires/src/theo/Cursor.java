05c7ef04f5cc42f32184d7e6e5e7b46a2ea17a90
rondine <theopozzi@gmail.com>
2014-01-17 16:54:48 +0100

    ajout de mon implémentation de curseur

---
+++ TestsUnitaires/src/theo/Cursor.java
@@ -0,0 +1,56 @@
+package theo;
+
+import specification.InterfaceCursor;
+import specification.InterfacePoint;
+
+public class Cursor implements InterfaceCursor {
+
+	@Override
+	public InterfacePoint getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InterfacePoint getDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPosition(InterfacePoint position) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setDirection(InterfacePoint direction) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stepStraigth() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void turnLeft() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void turnRight() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
