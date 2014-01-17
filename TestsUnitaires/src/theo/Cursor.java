dc34313ec4fa6acf8ace9177a67f1e9b854f76c3
rondine <theopozzi@gmail.com>
2014-01-17 17:03:29 +0100

    lol

--- TestsUnitaires/src/theo/Cursor.java
+++ TestsUnitaires/src/theo/Cursor.java
@@ -5,49 +5,49 @@ import specification.InterfacePoint;
 
 public class Cursor implements InterfaceCursor {
 
-	@Override
+	
 	public InterfacePoint getPosition() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+	
 	public InterfacePoint getDirection() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+	
 	public void setPosition(InterfacePoint position) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
+	
 	public void setDirection(InterfacePoint direction) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
+	
 	public void reset() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
+	
 	public void stepStraigth() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
+	
 	public void turnLeft() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
+	
 	public void turnRight() {
 		// TODO Auto-generated method stub
 
